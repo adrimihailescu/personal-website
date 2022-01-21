bf36f79e2cba380b58a3a66a341e7ac542bede58
adriana <adriana.mihalescu@gmail.com>
2022-01-21 21:21:41 +0000

    working forms with hubspot wuhuuu + sample env file

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import {
   ContactWrapper,
   FormWrapper,
@@ -37,14 +37,22 @@ const pageQuery = graphql`
             name
             required
             fieldType
+            placeholder
+            enabled
           }
         }
+        inlineMessage
       }
     }
   }
 `
 
 const Contact = () => {
+  const [formData, setFormData] = useState({})
+  const [formsSubmitStatus, setFormsSubmitStatus] = useState({
+    success: false,
+    fail: false,
+  })
   const data = useStaticQuery(pageQuery)
   const { title, subtitle, description } =
     data.allContentfulContactPage.nodes[0]
@@ -54,55 +62,112 @@ const Contact = () => {
     name: formName,
     submitText: formSubmitText,
     formFieldGroups,
+    inlineMessage: formSubmitSuccess,
   } = data.allHubspotForm.nodes[0]
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const hubSpotURL = `${process.env.HUBSPOT_ENDPOINT}/${process.env.HUBSPOT_PORTAL_ID}/${process.env.HUBSPOT_FORM_ID}`
+
+    const formDataFields = Object.keys(formData).map(key => ({
+      name: key,
+      value: formData[key],
+    }))
+
+    const data = {
+      fields: formDataFields,
+      context: {
+        pageUri: process.env.HUBSPOT_FORM_PAGE_URI,
+        pageName: process.env.HUBSPOT_PAGE_NAME,
+      },
+    }
+
+    const response = await fetch(hubSpotURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+
+    if (response.status === 200 && response.ok) {
+      setFormsSubmitStatus({ fail: false, success: true })
+    }
+
+    if (response.status === 403 || response.status === 404 || !response.ok) {
+      setFormsSubmitStatus({ fail: true, success: false })
+    }
+
+    setFormData({})
+  }
+
+  const handleChange = e => {
+    e.preventDefault()
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
 
   return (
     <ContactWrapper>
       <Title>{title}</Title>
       <h3>{subtitle}</h3>
       <p>{description}</p>
-      <FormWrapper>
-        <form>
-          {formFieldGroups.map(field => {
-            const fields = field.fields[0]
-            const isRequired = fields.required
-            switch (fields.fieldType) {
-              case "textarea":
-                return (
-                  <Label key={nanoid()}>
-                    <span>
-                      {fields.label}
-                      {isRequired ? "*" : null}
-                    </span>
-                    <Textarea
-                      type={fields.fieldType}
-                      required
-                      name={fields.name}
-                      required={isRequired}
-                    />
-                  </Label>
-                )
+      <form onSubmit={handleSubmit}>
+        {formFieldGroups.map(field => {
+          const fields = field.fields[0]
+          const isRequired = fields.required
 
-              default:
-                return (
-                  <Label key={nanoid()}>
-                    <span>
-                      {fields.label}
-                      {isRequired ? "*" : null}
-                    </span>
-                    <Input
-                      type={fields.fieldType}
-                      required
-                      name={fields.name}
-                      required={isRequired}
-                    />
-                  </Label>
-                )
-            }
-          })}
-          <Button type="submit">{formSubmitText}</Button>
-        </form>
-      </FormWrapper>
+          if (!fields.enabled) return
+
+          switch (fields.fieldType) {
+            case "textarea":
+              return (
+                <Label key={`form-${fields.name}`}>
+                  <span>
+                    {fields.label}
+                    {isRequired ? "*" : null}
+                  </span>
+                  <Textarea
+                    type={fields.fieldType}
+                    required
+                    name={fields.name}
+                    required={isRequired}
+                    onChange={handleChange}
+                    value={formData[fields.name] || ""}
+                    placeholder={fields.placeholder}
+                  />
+                </Label>
+              )
+
+            default:
+              return (
+                <Label key={`form-${fields.name}`}>
+                  <span>
+                    {fields.label}
+                    {isRequired ? "*" : null}
+                  </span>
+                  <Input
+                    type={fields.fieldType}
+                    required
+                    name={fields.name}
+                    required={isRequired}
+                    onChange={handleChange}
+                    value={formData[fields.name] || ""}
+                    placeholder={fields.placeholder}
+                  />
+                </Label>
+              )
+          }
+        })}
+        <Button type="submit">{formSubmitText}</Button>
+      </form>
+      {formsSubmitStatus.success ? (
+        <p style={{ color: "lime" }}>{formSubmitSuccess}</p>
+      ) : null}
+      {formsSubmitStatus.fail ? (
+        <p style={{ color: "red" }}>
+          Error submiting form, please try again later
+        </p>
+      ) : null}
     </ContactWrapper>
   )
 }
