a965c2db2250faef3982b8a16648dd4f88c7c298
adriana <adriana.mihalescu@gmail.com>
2022-01-24 15:51:49 +0000

    re-arranged components structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,12 @@
 import React from "react"
 import Layout from "../components/Layout/Layout"
-import AboutMe from "../components/About/About"
-import Projects from "../components/Projects/Projects"
-import Contact from "../components/Contact/Contact"
+
 import MainPage from "../components/MainPage/MainPage"
 // import Footer from "../components/Footer/Footer"
 
 export default function Home() {
   return (
     <Layout>
-      {/* <AboutMe />
-    //   <Projects />
-    //   <Contact /> */}
       <MainPage />
       {/* <Footer /> */}
     </Layout>
