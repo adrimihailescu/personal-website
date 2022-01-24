a5ce202893937ea63d3f50c741e449a84505b116
adriana <adriana.mihalescu@gmail.com>
2022-01-24 14:59:54 +0000

    added new component, delete unused folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,8 @@ import Layout from "../components/Layout/Layout"
 import AboutMe from "../components/About/About"
 import Projects from "../components/Projects/Projects"
 import Contact from "../components/Contact/Contact"
-import MainPage from "../experiments/mainPage"
+import MainPage from "../components/MainPage/MainPage"
+// import Footer from "../components/Footer/Footer"
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
     //   <Projects />
     //   <Contact /> */}
       <MainPage />
+      {/* <Footer /> */}
     </Layout>
   )
 }
