75cb1502831b986f78b29c712991698662a4bdf4
adriana <adriana.mihalescu@gmail.com>
2022-01-28 18:52:54 +0000

    add margin and padding to input, label and textarea

--- src/components/PagesComponents/Contact/Contact.style.js
+++ src/components/PagesComponents/Contact/Contact.style.js
@@ -3,10 +3,14 @@ import stylesVariables from "../../../sharedStyles"
 
 export const FormPanelContact = styled.form`
   /* width: 600px; */
+  box-sizing: border-box;
   display: flex;
   flex-direction: column;
   color: ${stylesVariables.colors.mainText};
-  margin: 50px;
+  margin: 40px;
+  padding: 10px;
+  background-color: #923cb5;
+  background-image: linear-gradient(147deg, #923cb5 0%, #000000 74%);
 `
 
 export const ParagraphPanelContact = styled.p`
@@ -15,33 +19,39 @@ export const ParagraphPanelContact = styled.p`
 
 export const LabelPanelContact = styled.label`
   display: block;
+  margin: 10px;
+  margin-bottom: 10px;
 `
 
 export const InputPanelContact = styled.input`
   border-radius: 10px;
+  border: none;
   display: block;
   padding: 10px;
   width: 100%;
-  margin: 20px;
-  overflow: hidden;
+  margin: 0 auto;
+  /* overflow: hidden; */
 `
 
 export const TextareaPanelContact = styled.textarea`
   border-radius: 10px;
+  border: none;
   display: block;
   padding: 10px;
   width: 100%;
-  margin: 20px;
+  margin: 0 auto;
 `
 
 export const ButtonPanelContact = styled.button`
   display: inline-block;
-  padding: 0.3em 1.2em;
+  padding: 10px;
+  margin-top: 10px;
   margin: 0 0.1em 0.1em 0;
-  border: 0.16em solid rgba(255, 255, 255, 0);
+  border: none;
   border-radius: 2em;
   box-sizing: border-box;
   text-decoration: none;
   background-color: ${stylesVariables.colors.mainText};
   align-self: center;
+  color: ${stylesVariables.colors.primary80};
 `
