d3cfad44d05a936962b95231e61f821c983779b6
adriana <adriana.mihalescu@gmail.com>
2022-02-10 15:12:04 +0000

    add border and media query to about and contact form

--- src/components/PagesComponents/About/About.style.js
+++ src/components/PagesComponents/About/About.style.js
@@ -5,9 +5,36 @@ export const DivPanelAbout = styled.div`
   // content in About
   color: ${stylesVariables.colors.mainText};
   margin: 0 auto;
-  max-width: 100%;
+  max-width: 400px;
   display: block;
   padding: 10px;
   border-bottom: 2px;
   text-shadow: 2px 2px 2px #37338c, 3px 3px 3px #37338c;
+  border-top: 10px solid ${stylesVariables.colors.grey};
+  border-bottom: 10px solid ${stylesVariables.colors.grey};
+  border-radius: 40px;
+
+  @media (min-width: ${stylesVariables.media.tablet}) {
+    max-width: 500px;
+    border-left: 0;
+    border-right: 0;
+    border-top: 10px solid ${stylesVariables.colors.grey};
+    border-bottom: 10px solid ${stylesVariables.colors.grey};
+  }
+
+  @media (min-width: ${stylesVariables.media.screen}) {
+    max-width: 600px;
+    border-left: 0;
+    border-right: 0;
+    border-top: 10px solid ${stylesVariables.colors.grey};
+    border-bottom: 10px solid ${stylesVariables.colors.grey};
+  }
+
+  @media (min-width: ${stylesVariables.media.bigScreen}) {
+    max-width: 800px;
+    border-left: 0;
+    border-right: 0;
+    border-top: 10px solid ${stylesVariables.colors.grey};
+    border-bottom: 10px solid ${stylesVariables.colors.grey};
+  }
 `
--- src/components/PagesComponents/Contact/Contact.style.js
+++ src/components/PagesComponents/Contact/Contact.style.js
@@ -2,22 +2,31 @@ import styled, { css } from "styled-components"
 import stylesVariables from "../../../sharedStyles"
 
 export const Form = styled.form`
-  box-sizing: border-box;
+  /* box-sizing: border-box; */
   display: flex;
   flex-direction: column;
   color: ${stylesVariables.colors.mainText};
   margin: 0 auto;
   padding: 10px;
-  max-width: 350px;
-  box-shadow: 0px 0px 12px -1px ${stylesVariables.colors.mainColor};
+  max-width: 320px;
+  border-top: 10px solid ${stylesVariables.colors.grey};
+  border-bottom: 10px solid ${stylesVariables.colors.grey};
   border-radius: 40px;
 
   @media (min-width: ${stylesVariables.media.tablet}) {
-    max-width: 550px;
+    max-width: 500px;
+    border-top: 0;
+    border-bottom: 0;
+    border-left: 10px solid ${stylesVariables.colors.grey};
+    border-right: 10px solid ${stylesVariables.colors.grey};
   }
 
   @media (min-width: ${stylesVariables.media.bigScreen}) {
     max-width: 600px;
+    border-top: 0;
+    border-bottom: 0;
+    border-left: 10px solid ${stylesVariables.colors.grey};
+    border-right: 10px solid ${stylesVariables.colors.grey};
   }
 `
 
@@ -49,13 +58,14 @@ export const Textarea = styled.textarea`
   padding: 10px;
   width: 100%;
   margin: 0 auto;
+  margin-bottom: 10px;
 `
 
 export const ButtonPanelContact = styled.button`
   display: inline-block;
   padding: 10px;
   margin-top: 10px;
-  margin: 0 0.1em 0.1em 0;
+  margin-bottom: 10px;
   border: none;
   border-radius: 30px;
   box-sizing: border-box;
@@ -64,6 +74,7 @@ export const ButtonPanelContact = styled.button`
   align-self: center;
   color: ${stylesVariables.colors.primary80};
   position: relative;
+  cursor: pointer;
 
   &:before {
     z-index: 0;
